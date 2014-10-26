15583537fdc1b2f08e8bbf2db7b515cdfea8ba5f
kyle roux <kyle@level2designs.com>
2014-10-25 23:58:24 -0700

    added import to fix #3, ColumnForm implemented

--- flask_cms/core/forms.py
+++ flask_cms/core/forms.py
@@ -22,3 +22,12 @@ class TestForm(Form):
     name = fields.StringField('name')
     
 
+class ColumnForm(Form):
+    col_id = fields.HiddenField()
+    column_width = fields.IntegerField('col width',description='width of columms per bootstrap3 grid')
+    push = fields.IntegerField('push',description='push column x col width to the right')
+    pull = fields.IntegerField('pull',description='pull column x col width to the left')
+    offset = fields.IntegerField('offset',description='place column offset x col width from normal placement')
+
+
+
