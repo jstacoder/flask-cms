8ebfc9ffbdc04d3eafcd3964ee7eb759149b88a5
kyle roux <kyle@level2designs.com>
2015-06-24 11:37:03 -0700

    removing debug statements

--- flask_cms/core/__init__.py
+++ flask_cms/core/__init__.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 from flask import Blueprint
 
-print 'imported as ',__name__
 core = Blueprint('core', __name__,
                  template_folder='templates',
                  url_prefix='/')
